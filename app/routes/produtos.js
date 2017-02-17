dcda00e199c8d8787bfa40f032c8ed297e2df7b2
Zeno França Junior <zfrancajunior@gmail.com>
2017-02-17 15:49:02 -0200

    Update database
    bases atualizadas

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,8 +1,6 @@
 module.exports = function(app){
 	app.get('/produtos',function(req,res){
 	res.render("produtos/lista");
-	res.render("produtos/img");
-	res.render("produtos/CSS");
 	});	
 	
 };
--- login.js
+++
@@ -1,8 +0,0 @@
-var http = require('http');
-
-var server = http.createServer(function(req,res){
-	res.end("<html><body><h1>Hello</h1></body></html>")
-});
-server.listen(3000);
-
-console.log('logado no sistema')
