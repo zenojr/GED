4605d644c42f23cdb6c93598668bc8aace34bb9a
Zeno França Junior <zfrancajunior@gmail.com>
2017-02-18 21:04:13 -0200

    bagunça 2
    bagunça 2

--- config/express.js
+++ config/express.js
@@ -1,14 +1,16 @@
-var app = require('express');
+var express = require('express');
 var load = require('express-load');
 
-var app =  express();
-var app_load = load();
-
-app.set('view engine','ejs');
-app.set('views','./app/views');
-console.log('Express Module Running!');
 //carregamento deixado fora da função para que o carregamento seja realizado apenas uma vez
 
 module.exports = function(){ //padrão nodejs para exportar modulos dentro do ecossistema
+	var app =  express();
+
+
+	app.set('view engine','ejs');
+app.set('views','./app/views');
+console.log('Express Module Running!');
+
+
 	return app;
 }
