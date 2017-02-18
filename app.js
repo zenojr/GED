a91ae41a97706c940de5d39aaa8a15df86fd89a3
Zeno França Junior <zfrancajunior@gmail.com>
2017-02-18 21:15:36 -0200

    configurando express load
    utilizando o express load

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 
 var app = require('./config/express')(); 
-var rotasProdutos = require('./app/routes/produtos')(app);
+//var rotasProdutos = require('./app/routes/produtos')(app); removido pelo uso do express load
 
 app.listen(3000,function(){ 
 	console.log("V8 running!!");
--- config/express.js
+++ config/express.js
@@ -4,12 +4,14 @@ var load = require('express-load');
 //carregamento deixado fora da função para que o carregamento seja realizado apenas uma vez
 
 module.exports = function(){ //padrão nodejs para exportar modulos dentro do ecossistema
-	var app =  express();
+
+	var app =  express(); //invocação do objeto
 
 
 	app.set('view engine','ejs');
-app.set('views','./app/views');
-console.log('Express Module Running!');
+	app.set('views','./app/views');
+	
+	load('routes').intro(app);//invocando objeto load
 
 
 	return app;
