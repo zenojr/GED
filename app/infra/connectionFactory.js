2799c6d51d7ebe38b5d2c8cb5401342d26bbe524
Zeno França Junior <zfrancajunior@gmail.com>
2017-02-18 22:36:28 -0200

    on load OK
    corrigido

--- app/infra/connectionFactory.js
+++ app/infra/connectionFactory.js
@@ -1,10 +1,15 @@
 var mysql = require('mysql');
-module.exports = function(){
-		return mysql.createConnection({
+
+var connectMYSQL = function(){
+	return mysql.createConnection({
 			host: 'localhost',
 			user: 'root',
 			password: 'pass@word1',
 			database: 'GED'
 		});
+}
+
+module.exports = function(){
+	return connectMYSQL;		
 
 }
--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,7 +1,7 @@
 
 module.exports = function(app){
 	app.get('/produtos',function(req,res){
-		var connection =  app.infra.connectionFactory;
+		var connection =  app.infra.connectionFactory();
 
 		connection.query('select * from livros',function(err,results){
 			res.render('produtos/lista',{lista:results});
