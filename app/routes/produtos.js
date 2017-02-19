602fe3fd4ad3af3391e81e5269c53d33e757bc9e
Zeno França Junior <zfrancajunior@gmail.com>
2017-02-18 22:24:44 -0200

    Arrumando bagunça
    corrigindo o connection factory

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -1,7 +1,7 @@
-//var connectionFactory = require('../infra/connectionFactory'); - removido devido ao uso do express load
+
 module.exports = function(app){
 	app.get('/produtos',function(req,res){
-		var connection = connectionFactory();
+		var connection =  app.infra.connectionFactory;
 
 		connection.query('select * from livros',function(err,results){
 			res.render('produtos/lista',{lista:results});
